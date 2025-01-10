546bcaf144fb0f15f4f0107f5f814b535f2399a5
Carlos Henrique <henriqueetec1987@gmail.com>
2025-01-09 23:27:26 -0300

    remove library and add coment

--- img_dimension.py
+++ img_dimension.py
@@ -1,14 +1,15 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
+from tkinter import Image
 
+# Abrir uma imagem existente
 img = Image.open('Imagem_colada.png')
+
+# Converter para escala de cinza
 img_gray = img.convert("L")
 img_gray.save("image_converted_gray.jpg")
 
+# Converter para preto e branco (binário)
 limiar = 128
 img_black_white = img_gray.point(lambda x: 255 if x > limiar else 0, '1')
-
 img_black_white.save("image_converted_black_white.jpg")
 
 print("Imagens salvas com sucesso!")
