62e439368075eb845ab53bb299fbc790e52a5e20
CARLOS HENRIQUE SCALAMBRINE DE SOUZA <135667372+DeveloperScalambrine@users.noreply.github.com>
2025-02-18 16:28:12 -0300

    done two functions to call websites

--- Assitent.py
+++ Assitent.py
@@ -1,6 +1,8 @@
 import gTTS
 import speech_recognition as sr
 import os
+import wikipedia
+import webbrowser
 
 text_convert = "Ola mundo em maquina de aprendizagem"
 def speak(text_convert):
@@ -23,3 +25,17 @@ def listening():
     except sr.RequestError as e:
         print("Unable to connect to speech recognition service: " + str(e))
         return ""
+
+text_open_wikipedia = "Alexa open wikipedia please"
+def open_wikipedia(text_open_wikipedia):
+    try:
+        result = wikipedia.summary(text_open_wikipedia)
+        speak(result)
+    except wikipedia.exceptions.PageError:
+        speak("I couldn't find anything about this on Wikipedia.")
+    except wikipedia.exceptions.DisambiguationError as e:
+        speak("I found several pages about this. Be more specific.")
+
+text_open_wikipedia = "Alexa open youtube please"
+def open_youtube():
+    webbrowser.open("https://www.youtube.com/")
